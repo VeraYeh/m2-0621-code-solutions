00c4ad68c4f02835190fd74479d9b07787d98f2b
Vera Yeh <brisa8281@gmail.com>
2021-06-23 14:51:19 -0700

    Completed javascript-constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,35 @@
+var anObject = new Object();
+
+console.log('value of anObject: ', anObject);
+
+console.log('typeof anObject: ', typeof anObject);
+
+var anArray = new Array();
+
+console.log('value of anArray: ', anArray);
+
+console.log('typeof anArray: ', typeof anArray);
+
+var aFunction = new Function();
+
+console.log('value of aFunction: ', aFunction);
+
+console.log('typeof aFunction: ', typeof aFunction);
+
+var aString = new String();
+
+console.log('value of aString: ', aString);
+
+console.log('typeof aString: ', typeof aString);
+
+var aNumber = new Number();
+
+console.log('value of aString: ', aNumber);
+
+console.log('typeof aString: ', typeof aNumber);
+
+var aBoolean = new Boolean();
+
+console.log('value of aString: ', aBoolean);
+
+console.log('typeof aString: ', typeof aBoolean);

79d72ec09113e67a4737f1696e20b3e924e22d0f
Vera Yeh <brisa8281@gmail.com>
2021-07-07 15:54:22 -0700

    Completed javascript-ajax.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+var $userList = document.querySelector('#user-list');
+
+function getUserData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function() {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    for (let i = 0; i < xhr.response.length; i++ ) {
+      var listItems = document.createElement('li');
+      listItems.textContent = xhr.response[i].name;
+      $userList.appendChild(listItems);
+    }
+  })
+  xhr.send();
+}
+getUserData();
